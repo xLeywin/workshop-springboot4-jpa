5d1b72575b678eb552dfa42ce87041aca5b33c21
xLeywin <wendellylan@gmail.com>
2025-12-12 15:33:54 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/wendellyv/course/entities/User.java
+++ src/main/java/com/wendellyv/course/entities/User.java
@@ -1,12 +1,17 @@
 package com.wendellyv.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,6 +24,10 @@ public class User implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name, email, phone, password;
+	
+	@JsonIgnore // Ignore loops
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders =  new ArrayList<>();
 	
 	
 	public User() {
@@ -73,6 +82,10 @@ public class User implements Serializable{
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -89,7 +102,5 @@ public class User implements Serializable{
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
-	}
-	
-	
+	}	
 }
